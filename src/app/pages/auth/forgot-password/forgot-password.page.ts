2d48c9eff54ef3387dd5588c9a39074817c49abd
Matias Gonzalez <matias.r.gozalez@gmail.com>
2023-10-19 00:33:42 -0300

    forgot-passward completed

--- src/app/pages/auth/forgot-password/forgot-password.page.ts
+++ src/app/pages/auth/forgot-password/forgot-password.page.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/models/user.model';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-forgot-password',
@@ -7,9 +11,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotPasswordPage implements OnInit {
 
-  constructor() { }
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+  })
+
+
+  firebaseSvc = inject(FirebaseService);
+  UtilsSvc = inject(UtilsService);
 
   ngOnInit() {
   }
+
+  async submit() {
+
+    const loading = await this.UtilsSvc.loading();
+    await loading.present();
+
+    if (this.form.valid) {
+
+      this.firebaseSvc.sendRecoveryEmail(this.form.value.email).then(res => {
+
+      }).catch(error => {
+        console.log(error);
+
+        this.UtilsSvc.presentToas({
+          message: error.massage,
+          duration: 2500,
+          color: 'primary',
+          position: 'middle',
+          icon: 'alert-circle-outline'
+        })
+
+      }).finally(() => {
+        loading.dismiss();
+      })
+    }
+
+  }
+
+  
 
 }
--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,9 +1,10 @@
 import { Injectable, inject } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile, sendPasswordResetEmail } from 'firebase/auth';
 import { User } from '../models/user.model';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import {getFirestore, setDoc, doc, getDoc} from '@angular/fire/firestore';
+import { retry } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -33,8 +34,14 @@ export class FirebaseService {
   updateUser(displayName: string){
     return updateProfile(getAuth().currentUser, {displayName})
   }
+
+  //-------- Enviar mail para restablecer contraseña -------
+
+  sendRecoveryEmail(email: string){
+    return sendPasswordResetEmail(getAuth(), email);
+  }
   
-  //---------------------------------------- BBDD ----------------------------------------
+  //---------------------------------------- Base de Datos  ----------------------------------------
   //--------setear un documento--------
   setDocument(path: string, data: any){
     return setDoc(doc(getFirestore(),path),data);
@@ -45,4 +52,6 @@ export class FirebaseService {
   async getDocument(path: string){
     return (await getDoc(doc(getFirestore(),path))).data();
   }
+
+
 }
