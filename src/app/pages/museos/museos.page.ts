e8e8ca7913abc1b22f8df00133280b49450a082d
Matias Gonzalez <matias.r.gozalez@gmail.com>
2023-11-07 12:17:27 -0300

    prueba para modificar api y funcione en andoid studio

--- src/app/pages/museos/museos.page.ts
+++ src/app/pages/museos/museos.page.ts
@@ -16,7 +16,7 @@ export class MuseosPage implements OnInit {
 
     //Obtiene info de los museos (api) mediante GET
     //Agregamos <any> a  this.http.get para evitar "Propery 'results' does not exist on typo 'object'."
-    this.http.get<any>('https://www.cultura.gob.ar/api/v2.0/museos/').subscribe(res =>{
+    this.http.get<any>('https://www.cultura.gob.ar/api/v2.0/organismos/').subscribe(res =>{
       console.log(res); //Muestra el array
       //ITEM B - Lo colocamos dentro de una variable array para luego recorrerla 
       this.museos = res.results;
