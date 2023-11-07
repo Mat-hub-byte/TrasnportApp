09530a7540eddbd6513bb258a46f36f57ae1b0f5
Matias Gonzalez <matias.r.gozalez@gmail.com>
2023-11-07 01:07:55 -0300

    package http-cordova

--- src/app/pages/museos/museos.page.ts
+++ src/app/pages/museos/museos.page.ts
@@ -18,7 +18,6 @@ export class MuseosPage implements OnInit {
     //Agregamos <any> a  this.http.get para evitar "Propery 'results' does not exist on typo 'object'."
     this.http.get<any>('https://www.cultura.gob.ar/api/v2.0/museos/').subscribe(res =>{
       console.log(res); //Muestra el array
-
       //ITEM B - Lo colocamos dentro de una variable array para luego recorrerla 
       this.museos = res.results;
     })
